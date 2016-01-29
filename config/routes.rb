522ab9fd72ac4d3256e377e09556545799a0efa0
saffront <jiashern@hotmail.com>
2016-01-29 14:19:20 +0000

    revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   post 'welcome/subscribe' => 'welcome#subscribe'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   # Example of regular route:
